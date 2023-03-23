394abe0e87c534376a92eef90b1eefb18133d135
Alexander Rodriguez <alexrguez@gmail.com>
2023-03-23 23:06:10 +0000

    I didn't commit until now!

--- scripts/my-script.js
+++ scripts/my-script.js
@@ -1,16 +1,71 @@
 console.log("Initialising console");
 
+let stage = "1";
+let guessedLetters = [];
+const picture = document.querySelector("#hangmanPic");
+picture.src = "images/" + stage +".png";
 
-const titles = document.querySelectorAll('h2.collapsible');
+const secretWord = "Guessme";
+//Split to create an array and with the map method it swaps each one for his uppercase equivalent
+const arrSecretword = secretWord.split("").map(letter => letter.toUpperCase());
+let hiddenWord = arrSecretword.map(() => "-");
 
+const secretWordPanel = document.querySelector(".flex-container-secretWord")
+
+// Add the arrSecretword to the flex-container-secretWord
+hiddenWord.forEach(letter => {
+  const p = document.createElement('p');
+  p.textContent = letter;
+  p.classList.add("iskey");
+  secretWordPanel.appendChild(p);
+});
+
+
+const keys = document.querySelectorAll(".iskey");
   // Loop through the title elements and add a click event listener to each
-  titles.forEach(title => {
-    title.addEventListener('click', () => {
-      // Get the corresponding flex container element
-      const container = title.nextElementSibling;
-      // Toggle the collapsed class on the container element
-      container.classList.toggle('collapsed');
+  keys.forEach(key => {
+    key.addEventListener('click', () => {
+      
+      const pressedKey = key.textContent
+      console.log("Pressed " + pressedKey);
 
+
+      if (arrSecretword.includes(pressedKey)) {
+
+        guessedLetters.push(pressedKey);
+        console.log(guessedLetters);
+
+        for (let i = 0; i < arrSecretword.length; i++) {
+          if (arrSecretword[i] === pressedKey) {
+        
+            //change background on secret word
+            secretWordPanel.children[i].style.backgroundColor = "Green";
+            secretWordPanel.children[i].style.filter = "none";
+
+            //change letter in hiddenWord to correct letter
+            hiddenWord[i] = pressedKey;
+            secretWordPanel.children[i].textContent = pressedKey;
+
+            //Change background on the keyboard
+            key.classList.add("guessed");
+
+            if (hiddenWord.join("") === arrSecretword.join("")) {
+              console.log("Has ganado");}
+
+          }
+        }
+      }
+      else {
+        key.classList.add("wrong");
+        stage++;
+        console.log("Stage " + stage);
+        picture.src = "images/" + stage +".png";
+
+        if (stage === 10){console.log("Muerte")}}
+
+        //add/remove shadow effect on key
+    key.classList.add("pressed");
+    setTimeout(() => key.classList.remove("pressed"), 100);
+       
     });
   });
-
