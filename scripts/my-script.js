0b2ad15c679cb110da8149d45bbd08357d113681
Alexander Rodriguez <alexrguez@gmail.com>
2023-03-24 17:55:53 +0000

    The secret word is shown when they lose

--- scripts/my-script.js
+++ scripts/my-script.js
@@ -1,6 +1,3 @@
-console.log("Initialising console");
-
-
 let guessedLetters = [];
 let gameEnded = false;
 const picture = document.querySelector("#hangmanPic");
@@ -9,15 +6,14 @@ const message = document.querySelector("#message");
 let stage = 1;
 picture.src = "images/" + stage +".png";
 
-// const secretWord = words[Math.floor(Math.random() * words.length)];
-const secretWord ="papapapapa";
+const secretWord = words[Math.floor(Math.random() * words.length)];
+//const secretWord ="papapapapa";
 
 //Split to create an array and with the map method it swaps each one for his uppercase equivalent
 const arrSecretword = secretWord.split("").map(letter => letter.toUpperCase());
 let hiddenWord = arrSecretword.map(() => "-");
 
 const secretWordPanel = document.querySelector(".flex-container-secretWord")
-
 
 // Add the arrSecretword to the flex-container-secretWord
 hiddenWord.forEach(letter => {
@@ -60,7 +56,6 @@ const keys = document.querySelectorAll(".iskey");
 
             if (hiddenWord.join("") === arrSecretword.join("")) {
               picture.src = "images/win.gif";
-              console.log("Has ganado");
               message.innerText="You won with " + (stage+1) + " attempts. Carlton is happy. Press here for another game";
               gameEnded = true;
               message.classList.add("activeMessage");
@@ -72,14 +67,12 @@ const keys = document.querySelectorAll(".iskey");
       else {
 
         key.classList.add("wrong");
-        console.log (gameEnded);
         if (!gameEnded){stage++};
-        console.log("Stage " + stage);
         picture.src = "images/" + stage +".png";
         
      
         if (stage === 10){
-          message.innerText="You lost, press here for another game";
+          message.innerText = "You lost, the secret word was '" + secretWord + "'. Press here for another game.";
           message.classList.add("activeMessage");
           gameEnded = true;
                   
