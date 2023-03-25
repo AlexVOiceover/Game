d89380e0d12e36f60137040f4eaf3e9feb0a28c8
Alexander Rodriguez <alexrguez@gmail.com>
2023-03-25 00:02:37 +0000

    h1 Changes periodically

--- scripts/words.js
+++ scripts/words.js
@@ -1,7 +1,7 @@
 
-const messages = ["Come on!!", "Don't give up!", "This is supposed to be for kids","I don't want to die like this","Are you even trying?","Maybe English is not your first language...",
-"I am a bit unconfortable here","The rope is a bit raspy","Who is going to feed my dog?","Do you want to know about our lord?","Well... what a situation, right?",
-"Just press a key!","I just ordered some stuff on Amazon","Look behid you! a three headed monkey!","Is this how it ends? I was hoping for a more epic finale!",
+const messages = ["Come on!!", "Don't give up!", "I don't want to die","Are you even trying?",
+"I am a bit unconfortable","The rope is a bit raspy","I'm hungry","Well... what a situation",
+"Just press a key!","Look behid you!","Is this how it ends?",
 "Not like this!","Try, try again!","Don't be a quitter!","Keep fighting, hero!","One more chance, maybe?","You can do it!","Hang in there, literally!","Stay strong, my friend!",
 "Escape or perish trying!","The suspense is killing!"];
 
